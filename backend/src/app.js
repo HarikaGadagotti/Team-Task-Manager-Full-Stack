de211e0eb5cf508f7a7b78364f862e0f2cd204f1
HarikaGadagotti <harikagadagotti@gmail.com>
2026-05-19 10:42:19 +0530

    fix port binding

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,4 +28,4 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Backend running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Backend running on port ${PORT}`));
